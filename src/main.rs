7f1196080b32d6cf106fbbba5d1f9e62d1f2ca01
Hans Peter <latuskati@gmail.com>
2025-06-02 19:29:59 +0200

    ws

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,24 @@ mod reizen;
 mod cards;
 mod game;
 
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+use std::thread::spawn;
+use tungstenite::accept;
+
+fn main () {
+    let server = TcpListener::bind("0.0.0.0:39153").unwrap();
+    for stream in server.incoming() {
+        spawn (move || {
+            let mut websocket = accept(stream.unwrap()).unwrap();
+            loop {
+                let msg = websocket.read().unwrap();
+
+                // We do not want to send back ping/pong messages.
+                if msg.is_binary() || msg.is_text() {
+                    websocket.send(msg).unwrap();
+                }
+            }
+        });
+    }
 }
+
